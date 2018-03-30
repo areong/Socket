dd0f48b15b38f775199a931fcc5bf69c30df9c28
areong <areong6378@gmail.com>
2018-03-30 08:48:56 +0800

    Remove not running connection threads.

--- src/com/areong/socket/ConnectionThread.java
+++ src/com/areong/socket/ConnectionThread.java
@@ -56,6 +56,10 @@ class ConnectionThread extends Thread {
             }
         }
     }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
     
     public void stopRunning() {
         isRunning = false;
